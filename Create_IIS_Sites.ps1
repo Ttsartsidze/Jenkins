6d5f3c75111a90d309151dacb741cfb285b192d0
Ttsartsidze <60338769+Ttsartsidze@users.noreply.github.com>
2020-07-08 11:19:08 +0400

    Add files via upload

---
+++ Create_IIS_Sites.ps1
@@ -0,0 +1,25 @@
+﻿$WebSiteName = "BulkImporter.API"
+$FolderName = "BulkImporter"
+$Service = "Api"
+$Bindigs = "http/*:80:BulkImporter.03.Test.API.lb.ge"
+$PoolName = $WebSiteName
+$NewPath = "D:\$Service\$FolderName\Current"
+$LogPath = "D:\Logs\$Service\$FolderName"
+if (-not(Test-Path -Path $NewPath))
+{
+New-Item -Path $NewPath -ItemType directory
+}
+if (-not(Test-Path -Path $LogPath))
+{
+New-Item -Path $LogPath -ItemType directory
+}
+#check if the site exists
+if (Test-Path $WebSiteName -pathType container)
+{
+return
+}
+#create the site
+C:/Windows/System32/inetsrv/appcmd.exe add site /name:$WebSiteName /bindings:$Bindigs /physicalPath:$NewPath
+C:/Windows/System32/inetsrv/appcmd.exe add apppool /name:$WebSiteName
+C:/Windows/System32/inetsrv/appcmd.exe set app $PoolName/ /applicationPool:$WebSiteName
+C:/Windows/System32/inetsrv/appcmd.exe start apppool /apppool.name:$WebSiteName
---
+++ Create_Windows_Services.ps1
@@ -0,0 +1,18 @@
+﻿$serviceName = "LB.LMSCollectionProcessHandler"
+$FolderName = "AltaSoftLMSEventHandler"
+$RunFileExe ="CollectionProcessHandler.exe"
+$Service = "Windows-Services"
+$LogPath = "D:\Logs\$Service\$FolderName"
+$NewPath = "D:\$Service\$FolderName\Current"
+$running = Get-Service -display $serviceName -ErrorAction SilentlyContinue
+if (-not(Test-Path -Path $NewPath))
+{
+    New-Item -Path $NewPath -ItemType directory
+}
+if (-not(Test-Path -Path $LogPath))
+{
+    New-Item -Path $LogPath -ItemType directory
+}
+If ( -not $running )
+{sc.exe Create $serviceName binPath= "D:\$Service\$FolderName\Current\$RunFileExe" DisplayName= "$serviceName"}
+else { $serviceName+ " is installed."}
